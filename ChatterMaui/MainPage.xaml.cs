0231006a33afb747b66ca255228b0c1546c9ae8a
FormatException <brian@formatexception.com>
2024-05-27 15:13:52 -0500

    Add Maui Chatter client

---
+++ ChatterMaui/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ChatterMaui.ViewModels;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace ChatterMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage(IMessenger messenger)
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel(Dispatcher, messenger);
+        }
+    }
+
+}
---
+++ ChatterMaui/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using ChatterMaui.ViewModels;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.Logging;
+
+namespace ChatterMaui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            //Add in the messenger so our back-end can access it
+            builder.Services.AddSingleton<WeakReferenceMessenger>();
+            builder.Services.AddSingleton<IMessenger, WeakReferenceMessenger>(provider => provider.GetRequiredService<WeakReferenceMessenger>());
+
+            //builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton<MainPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
